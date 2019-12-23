382dcee8b8af339d1f12cc9e7c0f2e55153c8f6b
Iltotore <rafbodaha@gmail.com>
2019-12-23 12:01:09 +0100

    Refractoring project
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- src/main/java/fr/il_totore/lavadrop/task/ChildTask.java
+++
@@ -1,8 +0,0 @@
-package fr.il_totore.lavadrop.task;
-
-import org.gradle.api.Task;
-
-public interface ChildTask {
-
-    void run(Task parent);
-}
--- src/main/java/fr/il_totore/lavadrop/yaml/YamlSerializable.java
+++
@@ -1,6 +0,0 @@
-package fr.il_totore.lavadrop.yaml;
-
-public interface YamlSerializable {
-
-    void write(EditableYamlMappingBuilder builder);
-}
